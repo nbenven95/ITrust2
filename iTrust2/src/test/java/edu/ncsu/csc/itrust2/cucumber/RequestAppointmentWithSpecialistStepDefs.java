c203538413621cb8c5dc951907cf46ef3da22b25
Noah Benveniste <nnbenven@ncsu.edu>
2018-11-13 12:56:46 -0500

    Issue #50: Created the stepdefs java file, currently empty. Just pushing it now so I'm not doing it all at once when it's finished.

---
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/RequestAppointmentWithSpecialistStepDefs.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc.itrust2.cucumber;
+
+/**
+ * StepDefs to implement the scenarios in the RequestAppointmentWithSpecialist
+ * feature file.
+ *
+ * @author Noah Benveniste
+ */
+public class RequestAppointmentWithSpecialistStepDefs extends CucumberTest {
+
+    /** */
+    private final String baseUrl = "http://localhost:8080/iTrust2";
+
+}
